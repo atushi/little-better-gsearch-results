cd097fa6260fd2050c50822e69e2287fad3e5563
atushi <atushi.yamazaki@gmail.com>
2009-01-17 23:32:54 +0900

    Version 0.0.04

--- littlebettergsearchresults.user.js
+++ littlebettergsearchresults.user.js
@@ -5,43 +5,34 @@
 // @description    little better for google search results
 // @include        http://www.google.*/search*
 // @include        https://www.google.*/search*
-// @version        0.0.03
+// @version        0.0.04
 // ==/UserScript==
-
-myBody = document.getElementsByTagName("body")[0];
-myBodyElements = myBody.getElementsByTagName("table");
-myDivElements = myBody.getElementsByTagName("div");
+var body = document.getElementsByTagName("body")[0];
+var table = body.getElementsByTagName("table");
+var div = body.getElementsByTagName("div");
 this.scroll = function() { rsChange(); }
 window.addEventListener("scroll", this.scroll, false);
 
-
 function rsChange() {
-  // navbar
-  if (myDivElements) {
-    lastNavbar = 0;
-    for (var i = 0; i < myDivElements.length; i++) {
-      if (myDivElements[i].id == "navbar") {
-        lastNavbar = i;
-      }
+    // navbar
+    if (div) {
+        lastNavbar = 0;
+        for (var i = 0; i < div.length; i++) {
+            if (div[i].id == "navbar") lastNavbar = i;
+        }
+        if (lastNavbar > 0) div[lastNavbar].id = "lastnavbar";
     }
-    if (lastNavbar > 0) { myDivElements[lastNavbar].id = "lastnavbar"; }
-  }
-  deleteElement("navbar");
+    deleteElement("navbar");
 
-  // width
-  for (var i = 0; i < myBodyElements.length; i++) {
-    if (myBodyElements[i].className == "" && myBodyElements[i].align != "center") {
-      myBodyElements[i].width="60%";
+    // width
+    for (var i = 0; i < table.length; i++) {
+        if (table[i].className == "" && table[i].align != "center") table[i].width="60%";
     }
-  }
 }
 
-
 function deleteElement(idName) {
-  myDivElements = document.getElementById(idName);
-  if (myDivElements) {
-    myDivElements.parentNode.removeChild(myDivElements);
-  }
+    div = document.getElementById(idName);
+    if (div) div.parentNode.removeChild(div);
 }
 
 rsChange();
