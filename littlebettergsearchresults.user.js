989f6e844296f256d8649f82eb1373b1103b809b
atushi <atushi.yamazaki@gmail.com>
2009-01-17 23:25:32 +0900

    Version 0.0.02

--- littlebettergsearchresults.user.js
+++ littlebettergsearchresults.user.js
@@ -4,10 +4,19 @@
 // @description    little better for google search results
 // @include        http://www.google.*/search*
 // @include        https://www.google.*/search*
-// @version        0.0.01
+// @version        0.0.02
 // ==/UserScript==
-myBody = document.getElementsByTagName("body")[0]; 
+myBody = document.getElementsByTagName("body")[0];
 myBodyElements = myBody.getElementsByTagName("table");
-for (var i=0; i<myBodyElements.length-4; i++) {
-  if (myBodyElements[i].className=="") {myBodyElements[i].width="60%";}
+this.scroll = function() { rsChange(); }
+window.addEventListener("scroll", this.scroll, false);
+
+function rsChange() {
+  for (var i = 0; i < myBodyElements.length; i++) {
+    if (myBodyElements[i].className == "" && myBodyElements[i].align != "center") {
+      myBodyElements[i].width="60%";
+    }
+  }
 }
+
+rsChange();
