9f188a3a98caad3143d670651f2350b8e9629d44
atushi <atushi.yamazaki@gmail.com>
2009-04-25 04:41:34 +0900

    Version 0.0.09

--- littlebettergsearchresults.user.js
+++ littlebettergsearchresults.user.js
@@ -5,8 +5,25 @@
 // @description    little better for google search results
 // @include        http://www.google.*/search*
 // @include        https://www.google.*/search*
-// @version        0.0.08 
+// @version        0.0.09 
 // ==/UserScript==
+var DEFAULT_TEXT_BELOW_THE_TITLE_WIDTH = '60';
+var ORIGINAL_TEXT_BELOW_THE_TITLE_WIDTH = '42';
+var KEY_CODE = {
+    P:          80, // Page 
+    L:          76, // last
+    W:          87, // width of text below the title 
+    ZERO:       48,
+    ONE:        49,
+    TWO:        50,
+    THREE:      51,
+    FOUR:       52,
+    FIVE:       53,
+    SIX:        54,
+    SEVEN:      55,
+    EIGHT:      56,
+    NINE:       57
+};
 
 /* 
  * Scroll Event
@@ -16,8 +33,22 @@ var p = body.getElementsByTagName('p');
 this.scroll = function() { rsChange(); }
 window.addEventListener('scroll', this.scroll, false);
 var lastPage = '';
-function rsChange() {
-    // add page id
+var textBelowTheTitleWidth = {
+    then:       ORIGINAL_TEXT_BELOW_THE_TITLE_WIDTH,
+    now:        DEFAULT_TEXT_BELOW_THE_TITLE_WIDTH,
+    set:        function() {
+    for (var i = 0; i < document.styleSheets[0].cssRules.length; i++) {
+        if (document.styleSheets[0].cssRules[i].cssText.indexOf('.s') != -1
+         && document.styleSheets[0].cssRules[i].cssText.indexOf('max-width') != -1
+         && document.styleSheets[0].cssRules[i].cssText.indexOf(textBelowTheTitleWidth.then + 'em') != -1) {
+          document.styleSheets[0].cssRules[i].style.cssText =  document.styleSheets[0].cssRules[i].style.cssText.replace(textBelowTheTitleWidth.then, textBelowTheTitleWidth.now);
+        }
+    }    
+}
+
+};
+
+function setPageId() {
     if (p) {
         for (var i = 0; i < p.length; i++) {
             if (p.item(i).firstChild) {
@@ -30,70 +61,54 @@ function rsChange() {
             }
         }
     }
-
-    // width
-    for (var i = 0; i < document.styleSheets[0].cssRules.length; i++) {
-        if (document.styleSheets[0].cssRules[i].cssText.indexOf('.s') != -1
-         && document.styleSheets[0].cssRules[i].cssText.indexOf('max-width') != -1
-         && document.styleSheets[0].cssRules[i].cssText.indexOf('42em') != -1) {
-          document.styleSheets[0].cssRules[i].style.cssText =  document.styleSheets[0].cssRules[i].style.cssText.replace('42','60');
-        }
-    }    
-
 }
 
-/* 
- * keyCode
-/*--------------------*/
-KCODE = {
-    P:80, // Page 
-    L:76, // last
-    ZERO:48,
-    ONE:49,
-    TWO:50,
-    THREE:51,
-    FOUR:52,
-    FIVE:53,
-    SIX:54,
-    SEVEN:55,
-    EIGHT:56,
-    NINE:57
-};
+function rsChange() {
+    setPageId();
+    textBelowTheTitleWidth.set();
+}
 
 /* 
  * Key Down Event
 /*--------------------*/
-var idKey = '';
-window.addEventListener('keydown', function(e) { event((e.keyCode)? e.keyCode: e.charCode); }, false);
+window.addEventListener('keydown', function(e) { event((e.keyCode) ? e.keyCode : e.charCode); }, false);
 function event(keyCode) {
-    if (KCODE.P == keyCode) {
+
+    if (KEY_CODE.P == keyCode) {
         idKey = 'page' + idKey;
         if (focusFlag == 0) {
-            if (document.getElementById(idKey)) window.scrollTo(0,document.getElementById(idKey).offsetTop);
-            if ('page1' == idKey) window.scrollTo(0,document.getElementById('res').offsetTop);
+            if (document.getElementById(idKey)) window.scrollTo(0, document.getElementById(idKey).offsetTop);
+            if ('page1' == idKey) window.scrollTo(0, document.getElementById('res').offsetTop);
         }
         idKey = '';
-    } else if (KCODE.L == keyCode) {
+    } else if (KEY_CODE.W == keyCode) {
+        if (idKey != '') {
+            textBelowTheTitleWidth.then = textBelowTheTitleWidth.now;
+            textBelowTheTitleWidth.now = idKey;
+            textBelowTheTitleWidth.set();
+        }
+        idKey = '';
+    } else if (KEY_CODE.L == keyCode) {
         idKey = lastPage;
-    } else if (KCODE.ZERO == keyCode) {
+    } else if (KEY_CODE.ZERO == keyCode) {
         idKey = idKey + '0';
-    } else if (KCODE.ONE == keyCode) {
+    } else if (KEY_CODE.ONE == keyCode) {
         idKey = idKey + '1';
-    } else if (KCODE.TWO == keyCode) {
+    } else if (KEY_CODE.TWO == keyCode) {
         idKey = idKey + '2';
-    } else if (KCODE.THREE == keyCode) {
+    } else if (KEY_CODE.THREE == keyCode) {
         idKey = idKey + '3';
-    } else if (KCODE.FOUR == keyCode) {
+    } else if (KEY_CODE.FOUR == keyCode) {
         idKey = idKey + '4';
-    } else if (KCODE.FIVE == keyCode) {
+    } else if (KEY_CODE.FIVE == keyCode) {
         idKey = idKey + '5';
-    } else if (KCODE.SIX == keyCode) {
+    } else if (KEY_CODE.SIX == keyCode) {
         idKey = idKey + '6';
-    } else if (KCODE.SEVEN == keyCode) {
+    } else if (KEY_CODE.SEVEN == keyCode) {
         idKey = idKey + '7';
-    } else if (KCODE.EIGHT == keyCode) {
+    } else if (KEY_CODE.EIGHT == keyCode) {
         idKey = idKey + '8';
-    } else if (KCODE.NINE == keyCode) {
+    } else if (KEY_CODE.NINE == keyCode) {
         idKey = idKey + '9';
     } else {
         idKey = '';
